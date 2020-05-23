9b49fe6992fe01d366ac97917b04da84f6a2cf31
Evan Yang <evanyang966@163.com>
2020-05-23 10:33:45 +0800

    添加View的分类文件

---
+++ 新浪微博项目/新浪微博项目/Categories/UIView+FrameExtension.h
@@ -0,0 +1,27 @@
+//
+//  UIView+FrameExtension.h
+//  新浪微博项目
+//
+//  Created by Evan Yang on 2020/5/23.
+//  Copyright © 2020 Evan Yang. All rights reserved.
+//
+
+
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (FrameExtension)
+
+@property(nonatomic,assign) CGFloat x;
+@property(nonatomic,assign) CGFloat y;
+@property(nonatomic,assign) CGSize size;
+@property(nonatomic,assign) CGFloat width;
+@property(nonatomic,assign) CGFloat height;
+@property(nonatomic,assign) CGPoint origin;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 新浪微博项目/新浪微博项目/Categories/UIView+FrameExtension.m
@@ -0,0 +1,75 @@
+//
+//  UIView+FrameExtension.m
+//  新浪微博项目
+//
+//  Created by Evan Yang on 2020/5/23.
+//  Copyright © 2020 Evan Yang. All rights reserved.
+//
+
+#import "UIView+FrameExtension.h"
+
+
+
+@implementation UIView (FrameExtension)
+
+- (void)setX:(CGFloat)x{
+    CGRect frame = self.frame;
+    frame.origin.x = x;
+    self.frame = frame;
+}
+
+- (void)setY:(CGFloat)y{
+    CGRect frame = self.frame;
+    frame.origin.y = y;
+    self.frame = frame;
+}
+
+- (void)setWidth:(CGFloat)width{
+    CGRect frame = self.frame;
+    frame.size.width = width;
+    self.frame = frame;
+}
+
+- (void)setHeight:(CGFloat)height{
+    CGRect frame = self.frame;
+    frame.size.height = height;
+    self.frame = frame;
+}
+
+- (void)setSize:(CGSize)size{
+    CGRect frame = self.frame;
+    frame.size = size;
+    self.frame = frame;
+}
+
+- (void)setOrigin:(CGPoint)origin{
+    CGRect frame = self.frame;
+    frame.origin = origin;
+    self.frame = frame;
+}
+
+- (CGPoint)origin{
+    return self.frame.origin;
+}
+
+- (CGFloat)x{
+    return self.frame.origin.x;
+}
+
+- (CGFloat)y{
+    return self.frame.origin.y;
+}
+
+- (CGFloat)width{
+    return self.frame.size.width;
+}
+
+- (CGFloat)height{
+    return self.frame.size.height;
+}
+
+- (CGSize)size{
+    return self.frame.size;
+}
+
+@end
