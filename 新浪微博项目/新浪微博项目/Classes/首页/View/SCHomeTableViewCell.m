0e4d838dc98917d4e75102b72235480652524f38
Evan Yang <evanyang966@163.com>
2020-06-21 08:15:29 +0800

    创建新的SCHomeTableViewCell

---
+++ 新浪微博项目/新浪微博项目/Classes/首页/View/SCHomeTableViewCell.m
@@ -0,0 +1,31 @@
+//
+//  SCHomeTableViewCell.m
+//  新浪微博项目
+//
+//  Created by Evan Yang on 2020/6/21.
+//  Copyright © 2020 Evan Yang. All rights reserved.
+//
+
+#import "SCHomeTableViewCell.h"
+
+@implementation SCHomeTableViewCell
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier{
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        
+    }
+    return self;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
